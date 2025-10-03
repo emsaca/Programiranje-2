89d985b803f7c549f518ab22275324d99784d042
Ema Križnič <169265279+emsaca@users.noreply.github.com>
2025-10-03 11:35:44 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿namespace HanojskiStolpi
+{
+    internal class Program
+    {
+        //naloga 11
+        static void Main(string[] args)
+        {
+            Hanoi(2, "a", "c", "b");
+        }
+        static void Hanoi(int n, string zač, string končni, string pomoč)
+        {
+            if (n == 1)
+            {
+                Console.WriteLine("premakni obroč iz " + zač + " na " + končni);
+                return;
+            }
+            Hanoi(n - 1, zač, pomoč, končni);
+            Console.WriteLine("premakni obroč iz " + zač + " na " + končni);
+            Hanoi(n - 1, pomoč, končni, zač);
+        }
+        
+    }
+}
